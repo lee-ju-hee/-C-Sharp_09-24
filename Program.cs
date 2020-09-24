b44c9d60f6930fd68a7106f66936937c3c123017
lee-ju-hee <55524173+lee-ju-hee@users.noreply.github.com>
2020-09-24 17:04:58 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            //string ipt;
+
+            Console.WriteLine("Hello CSharp world!!!");
+            Console.Write("정수 입력 : ");
+            abc:
+            /*
+            ipt = Console.ReadLine();
+            num1 = int.Parse(ipt);
+            */
+            num1 = int.Parse(Console.ReadLine());
+            Console.Write("정수 입력 : ");
+            num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(num1 + "+" + num2 + "=" + (num1 + num2));
+            Console.WriteLine(num1 + "-" + num2 + "=" + (num1 - num2));
+            Console.WriteLine(num1 + "*" + num2 + "=" + (num1 * num2));
+            Console.WriteLine(num1 + "/" + num2 + "=" + (num1 / num2));
+            goto abc;
+        }
+    }
+}
